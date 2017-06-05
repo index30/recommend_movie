770aea2581b7444f0b38d77819207198aa402739
ranthy <realthingshakes25@gmail.com>
2017-06-06 00:40:44 +0900

    行列生成

--- extract_data.py
+++ extract_data.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 
+
 # データを集める
 # ex. {'ユーザ名': [[item1], [item2], ...]}
 def collect_data(f, rec_num):
@@ -16,13 +17,15 @@ def collect_data(f, rec_num):
     return dic
 
 
+# 各ユーザのジャンルごとの評価総数行列
 def make_mat(dic, mov):
     P = np.zeros((len(dic), 19))
+    # d:ユーザID
     for d in dic:
-        print(len(dic[d]))
+        # i_d = [アイテムID, 評価, タイムスタンプ]
         for i_d in dic[d]:
-            P[int(d)-1, :] += np.array(list(mov_list.iloc[int(i_d[0])-1, 5:24]))
-            # print(i_d[0])
+            # 映画の情報をベクトル化
+            P[int(d)-1, :] += np.array(list(mov_list.iloc[int(i_d[0])-1, 5:24]))*int(i_d[1])
     return P
 
 
@@ -36,10 +39,5 @@ if __name__ == '__main__':
         # 映画のリスト
         mov_list = pd.read_csv('ml-100k/u.item', sep='|',
                                encoding='latin-1', header=None)
-        # print(mov_list.iloc[1, 4:24])
-        # [print(x) for x in mov_list.iloc[1, 4:24]]
-        # print(np.array(list(mov_list.iloc[1, 5:24])))
-        # mov_dic = collect_data(i, 24)
         P = make_mat(user_dic, mov_list)
         print(P[0, :])
-        print(len(user_dic['1']))
